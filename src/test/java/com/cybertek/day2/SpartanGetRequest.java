bb5993e57dbce4c2652322c417d87c3e5e41d6d3
burcuhasbay <burcu.hasbay@gmail.com>
2024-01-30 14:43:32 +0300

    Ready for the first API TestCase

--- src/test/java/com/cybertek/day2/SpartanGetRequest.java
+++ src/test/java/com/cybertek/day2/SpartanGetRequest.java
@@ -1,5 +1,8 @@
 package com.cybertek.day2;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class SpartanGetRequest {
@@ -11,6 +14,26 @@ public class SpartanGetRequest {
 @Test
     public void test2(){
 
+    /*
+Given Accept type application/json
+When user send GET request to api/spartans endpoint
+Then statusCode must be 200
+And response Content Type must be application/json
+And response body should include spartan result
+
+     */
+
+    Response response = RestAssured.given().accept(ContentType.JSON).when()
+            .get(baseUrl + "/api/spartans");
+
+    //Printing status code from the response object
+    System.out.println("response.statusCode() = " + response.statusCode());
+
+    //Printing Content Type from the response object
+    System.out.println("response.contentType() = " + response.contentType());
+
+    //Printing the whole body
+     response.prettyPrint();
 
 
 }
