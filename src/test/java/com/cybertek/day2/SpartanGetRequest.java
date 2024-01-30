dc57145f07bbefa7985ac979ae7158b7eb746b87
burcuhasbay <burcu.hasbay@gmail.com>
2024-01-30 14:20:05 +0300

    Ready for the first API TestCase

--- src/test/java/com/cybertek/day2/SpartanGetRequest.java
+++ src/test/java/com/cybertek/day2/SpartanGetRequest.java
@@ -1,11 +1,18 @@
 package com.cybertek.day2;
 
+import org.junit.jupiter.api.Test;
+
 public class SpartanGetRequest {
 
 
     //According to the Spartans Documentation, this is how you get all the spartans /api/spartans
-    String url = "http://54.158.33.169:8000/api/spartans";
+    String baseUrl = "http://54.158.33.169:8000";
+
+@Test
+    public void test2(){
 
 
+
+}
 
 }
