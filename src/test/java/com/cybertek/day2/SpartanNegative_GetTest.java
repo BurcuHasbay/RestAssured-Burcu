c2950bee4e5c22bbea5334e76b4e52637f0b80c2
burcuhasbay <burcu.hasbay@gmail.com>
2024-01-31 13:22:02 +0300

    Static import implement with * in this new Class

---
+++ src/test/java/com/cybertek/day2/SpartanNegative_GetTest.java
@@ -0,0 +1,28 @@
+package com.cybertek.day2;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SpartanNegative_GetTest {
+
+
+    //It runs all the test cases inside this class,
+    //It executes one time before everything.
+    //So that you don't have to rewrite the same base URL for everything
+    @BeforeAll
+    public static void init(){
+        //Save BASE URL inside this variable so that we don't have to type http method every single time
+        RestAssured.baseURI = "http://54.158.33.169:8000";
+
+
+    }
+
+
+
+
+
+
+}
