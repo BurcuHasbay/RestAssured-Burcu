3fd8148d2de9c3d554f3b5d8384e179a9b9b5cf4
burcuhasbay <burcu.hasbay@gmail.com>
2024-02-05 14:28:25 +0300

    New class for PATH method, get request

---
+++ src/test/java/com/cybertek/day4/ORDS_ApiTest_withPATH.java
@@ -0,0 +1,20 @@
+package com.cybertek.day4;
+
+import com.cybertek.utilities.HR_Test_Base;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+public class ORDS_ApiTest_withPATH extends HR_Test_Base {
+
+    @DisplayName("GET request to countries with Path Method")
+    @Test
+    public void test1(){
+
+
+
+    }
+
+
+}
