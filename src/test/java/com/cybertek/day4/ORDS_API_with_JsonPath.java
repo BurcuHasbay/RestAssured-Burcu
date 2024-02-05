8a6008c95173d78bc20143549053307142478ce9
burcuhasbay <burcu.hasbay@gmail.com>
2024-02-05 17:43:16 +0300

    JsonPath examples with HR

---
+++ src/test/java/com/cybertek/day4/ORDS_API_with_JsonPath.java
@@ -0,0 +1,39 @@
+package com.cybertek.day4;
+
+import com.cybertek.utilities.HR_Test_Base;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+public class ORDS_API_with_JsonPath extends HR_Test_Base {
+    @DisplayName("GET request to Countries using Json Path")
+    @Test
+    public void test1(){
+
+            //NEITHER QUERY PARAMS NOR PATH PARAMS
+        //ONLY THE SHORTEST WAY TO GET ALL THE COUNTRIES
+        Response response = get("/countries");
+
+
+        //TO USE JSON PATH
+        //GET THE SECOND COUNTRY NAME WITH JSONPATH
+
+        //FIRST CREATE JsonPath OBJECT
+        JsonPath jsonPath = response.jsonPath();
+
+        String secondCountryId = jsonPath.getString("items[1].country_name");
+        System.out.println("secondCountryId = " + secondCountryId);
+
+        //GET ALL THE country ids
+        List<String> allCountriesId = jsonPath.getList("items.country_id");
+        System.out.println("allCountriesId = " + allCountriesId);
+
+
+    }
+
+}
