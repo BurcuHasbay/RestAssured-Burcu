8b7e64ed02e72365653cd9d6eade62f93edb7072
burcuhasbay <burcu.hasbay@gmail.com>
2024-02-05 14:47:26 +0300

    New class for PATH method, get request

--- src/test/java/com/cybertek/day4/ORDS_ApiTest_withPATH.java
+++ src/test/java/com/cybertek/day4/ORDS_ApiTest_withPATH.java
@@ -1,6 +1,8 @@
 package com.cybertek.day4;
 
 import com.cybertek.utilities.HR_Test_Base;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,6 +14,27 @@ public class ORDS_ApiTest_withPATH extends HR_Test_Base {
     @Test
     public void test1(){
 
+
+        Response response = given().accept(ContentType.JSON).and().
+                      queryParam("q", "{\"region_id\":2}").
+                when().get("/countries");
+
+
+        assertEquals(200,response.statusCode());
+
+        //HOW CAN I DIRECTLY GET THE LIMIT (IT IS 25)
+        //PRINT 'LIMIT' RESULT
+        //IN THE PATH PARAMETERS, YOU GIVE THE KEY "LIMIT", IT GIVES YOU  AUTOMATICALLY THE VALUE OF THE KEY
+        System.out.println("response.path(\"limit\") = " + response.path("limit"));
+
+
+        //PRINT 'HASMORE'
+        //JUST GIVE THE KEY
+        System.out.println("response.path(\"hasmore\") = " + response.path("hasmore"));
+
+        //PRINT FIRST COUNTRY ID
+        String firstCountryID = response.path("item[0].country_id");
+        System.out.println("firstCountryID = " + firstCountryID);
 
 
     }
