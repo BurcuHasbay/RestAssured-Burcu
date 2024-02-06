43e23abcf39f7b041b5d30187c267d311500817b
burcuhasbay <burcu.hasbay@gmail.com>
2024-02-06 13:58:52 +0300

    Cyber new base URI

---
+++ src/test/java/com/cybertek/utilities/Cyber_Base.java
@@ -0,0 +1,18 @@
+package com.cybertek.utilities;
+
+import org.junit.jupiter.api.BeforeAll;
+
+import static io.restassured.RestAssured.baseURI;
+
+public abstract class Cyber_Base {
+    //We make this class abstract because we can create object from this base class
+    @BeforeAll
+    public static void init(){
+        //Save BASE URL inside this variable so that we don't have to type http method every single time
+
+        baseURI = "api.training.cydeo.com/";
+
+
+    }
+
+}
