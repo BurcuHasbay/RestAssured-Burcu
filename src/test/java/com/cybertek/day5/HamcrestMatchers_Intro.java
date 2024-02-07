f41808799c20f189b97b67d333a0124f9e874e55
burcuhasbay <burcu.hasbay@gmail.com>
2024-02-07 13:44:22 +0300

    HamcrestMatchers-assert that With everyItem() method-//CHECK IF ALL THE NUMBERS ARE GREATER >THAN 0

--- src/test/java/com/cybertek/day5/HamcrestMatchers_Intro.java
+++ src/test/java/com/cybertek/day5/HamcrestMatchers_Intro.java
@@ -13,106 +13,93 @@ import static org.hamcrest.Matchers.*;
 public class HamcrestMatchers_Intro {
 
     @DisplayName("Assertion with Numbers")
-@Test
-    public void simpleTest1(){
+    @Test
+    public void simpleTest1() {
 
-    //look for the equality
-    assertThat(5+5, is(10));
+        //look for the equality
+        assertThat(5 + 5, is(10));
 
-    assertThat(5+5,equalTo(10));
+        assertThat(5 + 5, equalTo(10));
 
-    assertThat(5+5,is(equalTo(10)));
+        assertThat(5 + 5, is(equalTo(10)));
 
-    //HOW IS THE NEGATIVE VERSION OF IT?
-    assertThat(5+5,is(not(equalTo(9))));
-    //OR
-    assertThat(5+5,not(equalTo(9)));
+        //HOW IS THE NEGATIVE VERSION OF IT?
+        assertThat(5 + 5, is(not(equalTo(9))));
+        //OR
+        assertThat(5 + 5, not(equalTo(9)));
 
-    //Number comparison
-    //greaterThan
-    //greaterThanOrEqualTo
-    //lessThan
-    //lessThanOrEqualTo
+        //Number comparison
+        //greaterThan
+        //greaterThanOrEqualTo
+        //lessThan
+        //lessThanOrEqualTo
 
-    assertThat(5+5,is(greaterThan(9)));
+        assertThat(5 + 5, is(greaterThan(9)));
 
-}
+    }
 
     @DisplayName("Assertion with String")
     @Test
-    public void stringHamcrest(){
+    public void stringHamcrest() {
 
         //Checking for equality is same as numbers
 
         String text = "B22 is learning Hamcrest";
 
-        assertThat(text,is("B22 is learning Hamcrest"));
+        assertThat(text, is("B22 is learning Hamcrest"));
 
-        assertThat(text,equalTo("B22 is learning Hamcrest"));
+        assertThat(text, equalTo("B22 is learning Hamcrest"));
 
-        assertThat(text,is(equalTo("B22 is learning Hamcrest")));
+        assertThat(text, is(equalTo("B22 is learning Hamcrest")));
 
 
         //CHECK IF THIS TEXT STARTS WITH B22
-        assertThat(text,startsWith("B22"));
+        assertThat(text, startsWith("B22"));
 
         //Now do it in a case-insensitive manner
-        assertThat(text,startsWithIgnoringCase("b22"));
+        assertThat(text, startsWithIgnoringCase("b22"));
 
         //Check if it ends with .... or not
-        assertThat(text,endsWith("Hamcrest"));
+        assertThat(text, endsWith("Hamcrest"));
 
         //Check a word in the middle of the text
-        assertThat(text,containsString("learning"));
+        assertThat(text, containsString("learning"));
         //with ignoring case sensitivity
-        assertThat(text,containsStringIgnoringCase("LEARNING"));
+        assertThat(text, containsStringIgnoringCase("LEARNING"));
 
         String str = "  ";
 
         //TO CHECK if trimmed string is empty string
         //TRIM IT THEN CHECK IF IT IS EMPTY
-        assertThat(str.trim(),emptyString());
+        assertThat(str.trim(), emptyString());
 
         //Check if str above is blank
-        assertThat(str,blankString());
+        assertThat(str, blankString());
 
 
     }
 
     @DisplayName("Hamcrest for Collection")
     @Test
-    public void testCollection(){
-        List<Integer>listOfNumbers = Arrays.asList(1,4,5,6,32,54,66,77,45,23);
+    public void testCollection() {
+        List<Integer> listOfNumbers = Arrays.asList(1, 4, 5, 6, 32, 54, 66, 77, 45, 23);
 
         //CHECK IF THE list HAS SIZE OF (estimated number)
-        assertThat(listOfNumbers,hasSize(10));
+        assertThat(listOfNumbers, hasSize(10));
 
         //HOW CAN YOU VERIFY NUMBER 77 IS INSIDE THE LIST
-        assertThat(listOfNumbers,hasItem(77));
+        assertThat(listOfNumbers, hasItem(77));
 
         //HOW CAN YOU VERIFY THAT MORE THAN ONE NUMBER IS INSIDE THE LIST?
         //FOR EXAMPLE >>>> (77,5,23)
-        assertThat(listOfNumbers,hasItems(77,5,23));
+        assertThat(listOfNumbers, hasItems(77, 5, 23));
+
+        //CHECK IF ALL THE NUMBERS ARE GREATER >THAN 0
+        assertThat(listOfNumbers, everyItem(greaterThan(0)));
+        //OR
+        //assertThat(listOfNumbers,everyItem(containsString()));
+        //assertThat(listOfNumbers,everyItem(startsWith()));
+
+
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
