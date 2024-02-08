d787714585ea610ce4490838b1171d2906bf324f
burcuhasbay <burcu.hasbay@gmail.com>
2024-02-08 15:55:13 +0300

    To deserialize

--- src/test/java/com/cybertek/day5/JSON_to_Java_Test.java
+++ src/test/java/com/cybertek/day5/JSON_to_Java_Test.java
@@ -1,6 +1,7 @@
 package com.cybertek.day5;
 
 import com.cybertek.utilities.SpartanTEst_Base;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,7 +14,10 @@ public class JSON_to_Java_Test extends SpartanTEst_Base {
     @Test
     public void oneSpartanToMap(){
 
-
+        Response response = given().pathParam("id", 15)
+                            .when().get("/api/spartans({id}")
+                            .then()
+                             .statusCode(200).extract().response();
 
 
     }
