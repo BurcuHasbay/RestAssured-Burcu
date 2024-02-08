80114191b2797aa6dd6b305c08f10bf880750272
burcuhasbay <burcu.hasbay@gmail.com>
2024-02-08 15:48:01 +0300

    Converting Json To Java Collection

---
+++ src/test/java/com/cybertek/day5/JSON_to_Java_Test.java
@@ -0,0 +1,12 @@
+package com.cybertek.day5;
+
+import com.cybertek.utilities.SpartanTEst_Base;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.*;
+public class JSON_to_Java_Test extends SpartanTEst_Base {
+
+
+
+
+}
