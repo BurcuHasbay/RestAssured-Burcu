6e0fabfa1420315a4a513456eee0046c72c3d7e8
burcuhasbay <burcu.hasbay@gmail.com>
2024-02-08 15:57:22 +0300

    To deserialize

--- src/test/java/com/cybertek/day5/JSON_to_Java_Test.java
+++ src/test/java/com/cybertek/day5/JSON_to_Java_Test.java
@@ -15,7 +15,7 @@ public class JSON_to_Java_Test extends SpartanTEst_Base {
     public void oneSpartanToMap(){
 
         Response response = given().pathParam("id", 15)
-                            .when().get("/api/spartans({id}")
+                            .when().get("/api/spartans/({id}")
                             .then()
                              .statusCode(200).extract().response();
 
