dd8ea6c92915ad624b3e8c36d5545edc087ae641
burcuhasbay <burcu.hasbay@gmail.com>
2024-02-12 17:36:27 +0300

    POJO for Spartan Test

---
+++ src/test/java/com/cybertek/day6/Spartan_POJO_GETRequest_Test.java
@@ -0,0 +1,47 @@
+package com.cybertek.day6;
+
+import com.cybertek.utilities.SpartanTEst_Base;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.*;
+public class Spartan_POJO_GETRequest_Test extends SpartanTEst_Base {
+
+
+@DisplayName("GET one spartan and CONVERT it to SPARTAN OBJECT")
+    @Test
+    public void oneSpartanPojo(){
+
+/*
+First send a GET request to ONE spartan
+Save it inside Response Object
+Then create convert one spartan Object from spartanClass
+*/
+
+    Response response = given().accept(ContentType.JSON).
+            and().
+            pathParam("id", 15).
+            when()
+            .get("/api/spartans/{id}")
+            .then()
+            .statusCode(200).contentType("application/json")
+            .extract().response();
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+}
