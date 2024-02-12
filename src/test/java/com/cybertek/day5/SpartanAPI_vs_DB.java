7214f9fa344750259ef7e85dc7f11fc52ffa51d1
burcuhasbay <burcu.hasbay@gmail.com>
2024-02-12 12:47:21 +0300

    toString should be used

--- src/test/java/com/cybertek/day5/SpartanAPI_vs_DB.java
+++ src/test/java/com/cybertek/day5/SpartanAPI_vs_DB.java
@@ -54,10 +54,10 @@ public class SpartanAPI_vs_DB extends SpartanTEst_Base {
         //WHICH COULD BE ACTUAL?
         //******DATABASE IS ALWAYS THE EXPECTED VALUE*******
 
-        assertThat(apiMap.get("id"), is(dbMap.get("SPARTAN_ID")));
+        assertThat(apiMap.get("id").toString(), is(dbMap.get("SPARTAN_ID").toString()));
         assertThat(apiMap.get("name"),is(dbMap.get("NAME")));
         assertThat(apiMap.get("gender"),is(dbMap.get("GENDER")));
-        assertThat(apiMap.get("phone"),is(dbMap.get("PHONE")));
+        assertThat(apiMap.get("phone").toString(),is(dbMap.get("PHONE").toString()));
 
 
 
