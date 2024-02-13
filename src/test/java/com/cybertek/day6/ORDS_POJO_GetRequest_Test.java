de31418fafd95f3107d41c4c1605f95bf8b2468a
burcuhasbay <burcu.hasbay@gmail.com>
2024-02-13 17:21:17 +0300

    New Task with POJO

--- src/test/java/com/cybertek/day6/ORDS_POJO_GetRequest_Test.java
+++ src/test/java/com/cybertek/day6/ORDS_POJO_GetRequest_Test.java
@@ -1,6 +1,8 @@
 package com.cybertek.day6;
 
+import com.cybertek.Pojos.Region;
 import com.cybertek.utilities.HR_Test_Base;
+import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -19,6 +21,14 @@ public class ORDS_POJO_GetRequest_Test extends HR_Test_Base {
     @Test
     public void regionTest(){
 
+        JsonPath jsonPath = get("/regions").then().statusCode(200).contentType("application/json")
+                .extract().jsonPath();
+
+        Region R1 = jsonPath.getObject("items[0]", Region.class);
+
+        System.out.println(R1);
+        System.out.println("R1.getRegion_name() = " + R1.getRegion_name());
+        System.out.println("R1.getRegion_id() = " + R1.getRegion_id());
 
 
     }
