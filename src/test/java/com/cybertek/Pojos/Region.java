d8f19bf33831a9733cca28acc3670f3e086b6f69
burcuhasbay <burcu.hasbay@gmail.com>
2024-02-13 18:46:13 +0300

    JsonProperty in Jackson Annotations

--- src/test/java/com/cybertek/Pojos/Region.java
+++ src/test/java/com/cybertek/Pojos/Region.java
@@ -1,5 +1,6 @@
 package com.cybertek.Pojos;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -10,8 +11,8 @@ import java.util.List;
 @Setter
 @ToString
 public class Region {
-
-    private int region_id;
+    @JsonProperty("region_id")
+    private int regionId;
     private String region_name;
     private List<Link> links;
 
