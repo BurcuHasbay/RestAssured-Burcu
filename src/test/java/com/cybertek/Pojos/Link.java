81bb05682eef81658a2727721ad2761c8d4974ad
burcuhasbay <burcu.hasbay@gmail.com>
2024-02-13 18:31:19 +0300

    Project Lombok

--- src/test/java/com/cybertek/Pojos/Link.java
+++ src/test/java/com/cybertek/Pojos/Link.java
@@ -1,32 +1,14 @@
 package com.cybertek.Pojos;
 
-public class Link {
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@ToString
+public class Link {
 private String rel;
 private String href;
 
-    public String getRel() {
-        return rel;
-    }
-
-    public void setRel(String rel) {
-        this.rel = rel;
-    }
-
-    public String getHref() {
-        return href;
-    }
-
-    public void setHref(String href) {
-        this.href = href;
-    }
-
-
-    @Override
-    public String toString() {
-        return "Link{" +
-                "rel='" + rel + '\'' +
-                ", href='" + href + '\'' +
-                '}';
-    }
 }
