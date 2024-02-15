9421b386aaa6a7489f189f593ab957d47b85e1ec
burcuhasbay <burcu.hasbay@gmail.com>
2024-02-15 15:26:14 +0300

    assertion

--- src/test/java/com/cybertek/day6/ORDS_POJO_GetRequest_Test.java
+++ src/test/java/com/cybertek/day6/ORDS_POJO_GetRequest_Test.java
@@ -102,7 +102,7 @@ first 4 values to do verification, how can you only get 4 values
         //TO COMPARE THE RESULT WITH THE EXPECTED RESULT,CREATE EXPECTED EMPTY LIST
 
         List<Integer> expectedRegionsIds = Arrays.asList(1, 2, 3, 4);
-        List<String> expectedRegionsNames= Arrays.asList("Europe","Americas","Asia","iddle East and Africa");
+        List<String> expectedRegionsNames= Arrays.asList("Europe","Americas","Asia","Middle East and Africa");
 
         assertThat(regionsIds,is(expectedRegionsIds));
         assertThat(regionsNames,is(expectedRegionsNames));
