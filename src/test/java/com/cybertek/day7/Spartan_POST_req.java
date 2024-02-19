7994101d54382c27947a55fedf0b6ff2512e92c9
burcuhasbay <burcu.hasbay@gmail.com>
2024-02-19 12:24:32 +0300

    Post request with RestAssured library

--- src/test/java/com/cybertek/day7/Spartan_POST_req.java
+++ src/test/java/com/cybertek/day7/Spartan_POST_req.java
@@ -1,7 +1,12 @@
 package com.cybertek.day7;
 
 import com.cybertek.utilities.SpartanTEst_Base;
-
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.*;
 public class Spartan_POST_req extends SpartanTEst_Base {
 
     /*
@@ -21,6 +26,39 @@ public class Spartan_POST_req extends SpartanTEst_Base {
     "A spartan is Born!" message
     and same data what is posted gender : male ....
      */
+
+/*
+We are sending only Json body NOT any params
+
+ */
+
+    @Test
+    public void postMethod(){
+
+        String requestJsonBody = "{\n" +
+                "    \"gender\": \"Male\",\n" +
+                "    \"name\": \"Severus\",\n" +
+                "    \"phone\": 99949330313563\n" +
+                "}";
+
+
+        Response response = given().accept(ContentType.JSON).and()
+                .contentType(ContentType.JSON)
+                .body(requestJsonBody)
+                .when()
+                .post("/api/spartans");
+
+
+        assertThat(response.statusCode(),is(201));
+        assertThat(response.contentType(),is("application/json"));
+
+        String expectedResponseMessage = "A Spartan is Born!";
+
+        assertThat(response.path("success"),is(expectedResponseMessage));
+
+
+    }
+
 
 
 
