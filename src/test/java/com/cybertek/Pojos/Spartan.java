2db41ed076783eef29dd7566858de438bdf60032
burcuhasbay <burcu.hasbay@gmail.com>
2024-02-19 13:04:58 +0300

    Project LomBok

--- src/test/java/com/cybertek/Pojos/Spartan.java
+++ src/test/java/com/cybertek/Pojos/Spartan.java
@@ -10,6 +10,14 @@ package com.cybertek.Pojos;
    "gender": "Female"
    "phone": 1938695106
  */
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class Spartan {
 
 private String name;
@@ -18,51 +26,5 @@ private String name;
 
     private long phone;
     private int id;
-
-    //GETTER SETTER
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public long getPhone() {
-        return phone;
-    }
-
-    public void setPhone(long phone) {
-        this.phone = phone;
-    }
-
-    @Override
-    public String toString() {
-        return "Spartan{" +
-                "name='" + name + '\'' +
-                ", gender='" + gender + '\'' +
-                ", phone=" + phone +
-                ", id=" + id +
-                '}';
-    }
-
-
-
 
 }
