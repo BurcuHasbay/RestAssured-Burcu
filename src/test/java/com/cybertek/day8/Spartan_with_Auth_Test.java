6d0059dc9e05e7caaadd4b0431e0c8dab52d7ad1
burcuhasbay <burcu.hasbay@gmail.com>
2024-02-20 14:27:05 +0300

    Needed Imports were added

--- src/test/java/com/cybertek/day8/Spartan_with_Auth_Test.java
+++ src/test/java/com/cybertek/day8/Spartan_with_Auth_Test.java
@@ -1,10 +1,25 @@
 package com.cybertek.day8;
 
 import com.cybertek.utilities.Spartan_AuthTESTBase;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.*;
 
 public class Spartan_with_Auth_Test extends Spartan_AuthTESTBase {
 
+@DisplayName("GET /api/spartans as a public User (guest) expexting 401")
+    @Test
+    public void test1(){
 
+
+
+
+
+
+}
 
 
 }
