3d50ae579161734acfa46f3d988e700600d1594b
burcuhasbay <burcu.hasbay@gmail.com>
2024-02-23 15:10:46 +0300

    Spartan Auth with Xml format

---
+++ src/test/java/com/cybertek/day10/Spartan_with_XML.java
@@ -0,0 +1,40 @@
+package com.cybertek.day10;
+
+import com.cybertek.utilities.Spartan_AuthTESTBase;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.*;
+public class Spartan_with_XML extends Spartan_AuthTESTBase {
+
+     @DisplayName("GET request to /api/spartans and verify XML")
+    @Test
+    public void getSpartanXML(){
+
+         /*
+         We will ask for .xml response
+         Assert that statusCode is 200
+         Assert content Type is xml (we get xml response)
+         Verify first spartan name is Meade
+          */
+
+         given()
+                 .accept(ContentType.XML)
+                 .and()
+                 .auth().basic("admin","admin")
+                 .when()
+                 .get("/api/spartans")
+                 .then()
+                 .statusCode(200)
+                 .contentType("application/xml;charset=UTF-8") //verify we got xml through header
+                 .body("List.item[0].name",is("Meade"))
+                 .body("List.item[0].gender",is("Male"))
+                 .log().all();
+
+     }
+
+
+}
