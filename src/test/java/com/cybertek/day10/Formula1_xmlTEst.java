ea60cb4393abd13884a25a17df7be3ab7f192377
burcuhasbay <burcu.hasbay@gmail.com>
2024-02-23 16:09:03 +0300

    formula 1 XML test

--- src/test/java/com/cybertek/day10/Formula1_xmlTEst.java
+++ src/test/java/com/cybertek/day10/Formula1_xmlTEst.java
@@ -1,4 +1,17 @@
 package com.cybertek.day10;
 
+import org.junit.jupiter.api.BeforeAll;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.*;
 public class Formula1_xmlTEst {
+
+    @BeforeAll
+    public static void setUp(){
+
+
+        baseURI ="http://ergast.com";
+    }
+
 }
