b7d2ee344c230a3b62d94997c7808bc89e4974e0
burcuhasbay <burcu.hasbay@gmail.com>
2024-02-26 16:44:17 +0300

    Json Schema Validation Test

--- src/test/java/com/cybertek/day10/jsonSchemaValidation_test.java
+++ src/test/java/com/cybertek/day10/jsonSchemaValidation_test.java
@@ -15,7 +15,7 @@ public class jsonSchemaValidation_test {
 
         given().accept(ContentType.JSON)
                 .and().pathParam("id",10)
-                .auth().basic("adming","admin")
+                .auth().basic("admin","admin")
                 .when()
                 .get("/api/spartans/{id}")
                 .then()
