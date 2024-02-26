f3730ea33c6777cf5c3af0a2772e624bd09da4b7
burcuhasbay <burcu.hasbay@gmail.com>
2024-02-27 00:44:38 +0300

    Response Time test-both

--- src/test/java/com/cybertek/day10/ResponseTimeTest.java
+++ src/test/java/com/cybertek/day10/ResponseTimeTest.java
@@ -21,7 +21,7 @@ public class ResponseTimeTest extends Spartan_AuthTESTBase {
                 .accept(ContentType.JSON)
                 .when()
                 .get("/api/spartans")
-                        .then().time(lessThanOrEqualTo(2000L))
+                        .then().time(both(greaterThan(500L)).and(lessThan(2000L)))
                         .extract().response();
 
         //Get the Response inside the Response Object
@@ -29,6 +29,9 @@ public class ResponseTimeTest extends Spartan_AuthTESTBase {
         System.out.println("response.getTime() = " + response.getTime());
 
         //HOW CAN YOU VERIFY THAT THIS AMOUNT OF TIME IS BETWEEN the RANGE OF ??
+        //Should be ranging between some certain amount of time
+        //lessThan and greaterThan should be together
+        //both :D (both(greaterThan(500L)).and(lessThan(2000L)))
 
 
 
