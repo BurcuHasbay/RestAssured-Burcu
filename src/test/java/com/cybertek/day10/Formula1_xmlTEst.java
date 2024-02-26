fbc9da66d68157f4ef41586d16fe550f1e5584c1
burcuhasbay <burcu.hasbay@gmail.com>
2024-02-26 12:00:33 +0300

    formula 1 XML test

--- src/test/java/com/cybertek/day10/Formula1_xmlTEst.java
+++ src/test/java/com/cybertek/day10/Formula1_xmlTEst.java
@@ -1,6 +1,8 @@
 package com.cybertek.day10;
 
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
@@ -9,10 +11,25 @@ public class Formula1_xmlTEst {
 
     @BeforeAll
     public static void setUp(){
-    //http://ergast.com/api/f1/constructors/mclaren/circuits/monza/drivers
+    //http://ergast.com/api/f1/drivers/alonso
 
         baseURI ="http://ergast.com";
+        // api is always basePath for Spartans
         basePath = "/api/f1";
+
+    }
+
+    @Test
+    public void test1(){
+
+
+        Response response = given().pathParam("driver", "alonso")
+                .when().get("/drivers/{driver}")
+                .then()
+                .statusCode(200).log().all()
+                .extract().response();
+
+
 
     }
 
