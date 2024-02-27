e758926d130de5f6424e1ed6457d566ca1d38e9b
burcuhasbay <burcu.hasbay@gmail.com>
2024-02-27 18:20:05 +0300

    LifeCycle annotations-Parametrized-CsvSource

---
+++ src/test/java/com/cybertek/day11/CsvSource_ParametrizedTest.java
@@ -0,0 +1,29 @@
+package com.cybertek.day11;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class CsvSource_ParametrizedTest {
+
+    /*
+    Test first number + second number = third number
+    1,3,4
+    2,3,5
+    8,7,15
+    10,9,19
+     */
+
+    @ParameterizedTest
+    @CsvSource({"1, 3 , 4",
+                "2, 3 , 5",
+                "8, 7 , 15",
+                "10, 9 , 19"})
+    public void testAddition(int num1, int num2, int sum){
+
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+        System.out.println("sum = " + sum);
+    }
+
+}
