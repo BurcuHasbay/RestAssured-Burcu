68c0811bab10e100bcf8fbb4bdde5861640be340
burcuhasbay <burcu.hasbay@gmail.com>
2024-02-28 13:50:05 +0300

    Excel Practice with Vytrack excel

--- src/test/java/com/cybertek/day11/ExcelUtil_Practice.java
+++ src/test/java/com/cybertek/day11/ExcelUtil_Practice.java
@@ -1,4 +1,26 @@
 package com.cybertek.day11;
+import com.cybertek.utilities.ExcelUtil;
 
+import java.util.List;
+import java.util.Map;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.*;
 public class ExcelUtil_Practice {
+
+   public void test1(){
+
+       ExcelUtil vytrackFile = new ExcelUtil("src/test/resources/Vytracktestdata.xlsx","QA3-short");
+
+        //method for returning list Of Map
+       List<Map<String, String>> dataList = vytrackFile.getDataList();
+       for (Map<String, String> rowMap : dataList) {
+           System.out.println("rowMap = " + rowMap);
+       }
+
+
+   }
+
+
 }
