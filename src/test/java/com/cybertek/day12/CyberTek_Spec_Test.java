16d3c1116da8f49b24a12f482fa878f3627926ff
burcuhasbay <burcu.hasbay@gmail.com>
2024-02-29 13:46:44 +0300

    RequestSpecification and ResponseSpecification

---
+++ src/test/java/com/cybertek/day12/CyberTek_Spec_Test.java
@@ -0,0 +1,78 @@
+package com.cybertek.day12;
+
+import com.cybertek.utilities.Cyber_Base;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.*;
+
+public class CyberTek_Spec_Test extends Cyber_Base {
+
+
+    /*
+    All tests in this class will expect "application/json;charset=UTF-8"
+    in the request accept(ContentType.JSON)
+    all tests in this class, statusCode will be 200
+    all tests in this class, response content type in the Headers
+    is "application/json;charset=UTF-8"
+     */
+
+    /* Repeated parts in both tests: WHY? BECAUSE IT SHOULD BE REUSABLE
+    *accept-ContentType.Json
+    *Status-Code = 200
+    * Response ContentType in Headers = "application/json;charset=UTF-8"
+    *
+    **/
+    @Test
+    public void test1(){
+
+        RequestSpecification requestSpecification1 = given()
+                .accept(ContentType.JSON)
+                .and().log().all();
+
+        ResponseSpecification responseSpecification1 = expect().statusCode(200)
+                .and()
+                .contentType("application/json;charset=UTF-8")
+                        .logDetail(LogDetail.ALL);
+
+        given().
+                         spec(requestSpecification1)
+                .when()
+                        .get("/student/all")
+                .then()
+                        .spec(responseSpecification1);
+
+
+
+
+
+    }
+
+       @Test
+    public void test2(){
+
+        given()
+                .accept(ContentType.JSON)
+                .and()
+                .pathParam("id",4)
+                .when()
+                .get("/student/{id}")
+                .then()
+                .statusCode(200)
+                .and()
+                .contentType("application/json;charset=UTF-8");
+
+
+
+
+
+       }
+
+
+
+}
--- src/test/java/com/cybertek/utilities/Cyber_Base.java
+++ src/test/java/com/cybertek/utilities/Cyber_Base.java
@@ -1,17 +1,33 @@
 package com.cybertek.utilities;
 
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 import org.junit.jupiter.api.BeforeAll;
 
-import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.*;
 
 public abstract class Cyber_Base {
     //We make this class abstract because we can create object from this base class
+
+    public static RequestSpecification requestSpecification1;
+    public static ResponseSpecification responseSpecification1;
+
     @BeforeAll
     public static void init(){
         //Save BASE URL inside this variable so that we don't have to type http method every single time
 
         baseURI = "https://api.training.cydeo.com/";
 
+        requestSpecification1 = given()
+                .accept(ContentType.JSON)
+                .and().log().all();
+
+        responseSpecification1 = expect().statusCode(200)
+                .and()
+                .contentType("application/json;charset=UTF-8")
+                .logDetail(LogDetail.ALL);
 
     }
 
